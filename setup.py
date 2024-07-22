c148d61c34d8f4f6289045535b24753673fb6eec
Amrit Purshotam <amrit.purshotam@gmail.com>
2024-07-22 20:37:34 +0200

    Setup requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+
+import pkg_resources
+from setuptools import find_packages, setup
+
+
+def get_requirements(requirements_path: str):
+    with pathlib.Path(requirements_path).open() as requirements_txt:
+        requirements = [
+            str(requirement)
+            for requirement in pkg_resources.parse_requirements(requirements_txt)
+        ]
+    return requirements
+
+
+setup(
+    name="pythonseed",
+    packages=find_packages(include=["src"]),
+    version="1.0.0",
+    description="",
+    author="",
+    python_requires=">=3.11",
+    install_requires=get_requirements("requirements.txt"),
+    extras_require={
+        "dev": get_requirements("scripts/requirements-dev.txt"),
+        "notebook": get_requirements("scripts/requirements-nb.txt"),
+        "test": get_requirements("scripts/requirements-test.txt"),
+    },
+)
