c14cadd7b3dd42aa99a8d08ac309bf9751328774
Amrit Purshotam <amrit.purshotam@gmail.com>
2024-07-22 22:07:29 +0200

    Add sample code

--- pythonseed/helper.py
+++ pythonseed/helper.py
@@ -1,2 +1,12 @@
 def increment(x: int) -> int:
     return x + 1
+
+
+def bugbear_error():
+    try:
+        list1 = [1, 2]
+        list2 = [1, 2, 3]
+        for i, j in zip(list1, list2):  # show squiggly under zip for bugbear error
+            print(i, j)
+    except:  # show squiggly under except
+        print("bugbear")
